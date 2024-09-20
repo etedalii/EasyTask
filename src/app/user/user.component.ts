5b2462d76726f1997d5c341f742b5ce71bdf202d
etedali <etedali@gmail.com>
2024-09-19 21:46:57 -0400

    added task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,21 +1,38 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, signal, computed, Input, input, Output , output, EventEmitter} from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// type User = {
+//   id: string,
+//   name: string,
+//   avatar: string
+// }
+
+interface User {
+     id: string,
+     name: string,
+     avatar: string
+}
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  //setable from outside
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  // @Input({required: true}) id!: string;
+
+  @Input({required : true}) user!: User;
+
+  @Output() selectedUser = new EventEmitter<string>(); 
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
 
   onSelectUser() {
-    let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.selectedUser.emit(this.user.id); // Emits the 'id' as a string
   }
 }
